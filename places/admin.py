b791742d1dd14efa2139c6463b4f1123e51b981e
Raman <149716575+RamanSashyn@users.noreply.github.com>
2025-05-07 13:43:03 +0200

    Упростил добавление фотографий

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 from .models import Place, Image
 
+
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 1
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
+    inlines = [ImageInline]
     list_display = ('title',)
+    search_fields = ('title',)
+
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
     list_display = ('place', 'position')
+    list_filter = ('place',)
