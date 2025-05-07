36130284951a6df0b35dee59c2bc25bde628182e
Raman <149716575+RamanSashyn@users.noreply.github.com>
2025-05-07 13:51:24 +0200

    Добавил превью

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Place, Image
 
 
 class ImageInline(admin.TabularInline):
     model = Image
     extra = 1
+    readonly_fields = ('get_preview',)
+    fields = ('image', 'get_preview', 'position')
+
+    def get_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="max-height: 200px;"/>', obj.image.url)
+        return "Нет изображения"
+
+    get_preview.short_description = "Превью"
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
@@ -15,5 +25,11 @@ class PlaceAdmin(admin.ModelAdmin):
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ('place', 'position')
-    list_filter = ('place',)
+    list_display = ['place', 'position', 'preview']
+    readonly_fields = ['preview']
+    ordering = ['place', 'position']
+
+    def preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="max-height: 200px;"/>', obj.image.url)
+        return "Нет изображения"
