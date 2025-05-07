00ad06306090467b7160946408118dea2324b122
Raman <149716575+RamanSashyn@users.noreply.github.com>
2025-05-07 14:12:15 +0200

    Миграция для изменения формата поля

--- places/models.py
+++ places/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from tinymce.models import HTMLField
+
 
 class Place(models.Model):
     title = models.CharField('Название', max_length=200)
     short_description = models.TextField('Краткое описание', blank=True)
-    long_description = models.TextField('Полное описание', blank=True)
+    long_description = HTMLField('Полное описание', blank=True)
     latitude = models.FloatField('Широта', blank=True, null=True)
     longitude = models.FloatField('Долгота', blank=True, null=True)
 
