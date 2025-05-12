ab772a90b7df4632ff8a00974d45af7926a6011e
Raman <149716575+RamanSashyn@users.noreply.github.com>
2025-05-12 15:49:35 +0200

    Переместил views.py в другой app

--- places/views.py
+++ places/views.py
@@ -1,3 +1,54 @@
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
 from django.shortcuts import render
+from places.models import Place
+from django.urls import reverse
 
-# Create your views here.
+def index(request):
+    places = []
+
+    for place in Place.objects.all():
+        places.append({
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.longitude, place.latitude]
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": str(place.id),
+                "detailsUrl": reverse('place-details', args=[place.id]),
+            }
+        })
+
+    geojson = {
+        "type": "FeatureCollection",
+        "features": places
+    }
+
+    context = {
+        'places_geojson': geojson,
+    }
+
+    return render(request, 'index.html', context)
+
+
+def place_details(request, place_id):
+    place = get_object_or_404(Place, id=place_id)
+
+    place_data = {
+        "title": place.title,
+        "imgs": [image.image.url for image in place.images.all()],
+        "description_short": place.short_description,
+        "description_long": place.long_description,
+        "coordinates": {
+            "lng": place.longitude,
+            "lat": place.latitude,
+        }
+    }
+
+    return JsonResponse(
+        place_data,
+        json_dumps_params={"ensure_ascii": False, "indent": 2},
+    )
+
