6f9cccf5aaa6ad1ea527a993505a837b3daed9ff
Raman <149716575+RamanSashyn@users.noreply.github.com>
2025-05-12 16:07:24 +0200

    Сделал поля для координат обязательным

--- places/models.py
+++ places/models.py
@@ -6,8 +6,8 @@ class Place(models.Model):
     title = models.CharField('Название', max_length=200)
     short_description = models.TextField('Краткое описание', blank=True)
     long_description = HTMLField('Полное описание', blank=True)
-    latitude = models.FloatField('Широта', blank=True, null=True)
-    longitude = models.FloatField('Долгота', blank=True, null=True)
+    latitude = models.FloatField('Широта')
+    longitude = models.FloatField('Долгота')
 
     def __str__(self):
         return self.title
